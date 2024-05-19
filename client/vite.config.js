7fcb72c918a28caaeeed52671a9a4247a26c986b
subrrahmanyem <subbuthoto91@gmail.com>
2024-05-19 09:37:50 +0530

    newUp

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     port: 3000
   },
-  base: './', // or the correct path for your deployment
+  base: '/', // or the correct path for your deployment
   build: {
     outDir: 'dist',
   }
